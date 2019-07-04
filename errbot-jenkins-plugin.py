8fc0bf82aa274c003d4a1989234d523de1d0c8a3
Caio Almeida <caio.almeida@gympass.com>
2019-07-04 19:31:28 -0300

    Remove unused code

--- errbot-jenkins-plugin.py
+++ errbot-jenkins-plugin.py
@@ -2,25 +2,6 @@ from errbot import BotPlugin, botcmd, arg_botcmd, webhook
 
 
 class ErrbotJenkinsPlugin(BotPlugin):
-    """
-    Handle Jenkins using errbot.
-    """
-
-    def activate(self):
-        """
-        Triggers on plugin activation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(ErrbotJenkinsPlugin, self).activate()
-
-    def deactivate(self):
-        """
-        Triggers on plugin deactivation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(ErrbotJenkinsPlugin, self).deactivate()
 
     def get_configuration_template(self):
         """
@@ -31,40 +12,6 @@ class ErrbotJenkinsPlugin(BotPlugin):
         return {'EXAMPLE_KEY_1': "Example value",
                 'EXAMPLE_KEY_2': ["Example", "Value"]
                }
-
-    def check_configuration(self, configuration):
-        """
-        Triggers when the configuration is checked, shortly before activation
-
-        Raise a errbot.utils.ValidationException in case of an error
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(ErrbotJenkinsPlugin, self).check_configuration(configuration)
-
-    def callback_connect(self):
-        """
-        Triggers when bot is connected
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_message(self, message):
-        """
-        Triggered for every received message that isn't coming from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_botmessage(self, message):
-        """
-        Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
 
     @webhook
     def example_webhook(self, incoming_request):
@@ -78,9 +25,9 @@ class ErrbotJenkinsPlugin(BotPlugin):
         """A command which simply returns 'Example'"""
         return "Example"
 
-    @arg_botcmd('job-name', type=str)
+    @arg_botcmd('job', type=str)
     def jenkins_find_job(self, message, args):
-        return args.job_name
+        return args.job
 
 
     @arg_botcmd('name', type=str)
